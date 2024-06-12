348f27361c20db4c589d37041cb5c7d9cd5ff94a
cojarobietu9 <153288137+cojarobietu9@users.noreply.github.com>
2024-06-13 00:39:41 +0200

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,7 +7,7 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         CustomEventManager.initialize();
-        MazeDataSingleton.initialize();
+        MazeBrowse.initialize();
 
         SwingUtilities.invokeLater(() -> {
             setLookAndFeel();
